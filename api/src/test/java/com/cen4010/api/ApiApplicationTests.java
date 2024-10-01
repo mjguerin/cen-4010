f0a271184d8b7e372f34a49d75f8501e709067a5
cchue001 <cchue001@fiu.edu>
2024-09-30 22:56:28 -0400

    Add files via upload

---
+++ api/src/test/java/com/cen4010/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cen4010.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
