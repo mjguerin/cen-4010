f76535363707abd047a179b9507d0123712a8f01
cchue001 <cchue001@fiu.edu>
2024-11-29 22:50:25 -0500

    bookstore.sql

--- cen4010/bookstore.sql
+++ cen4010/bookstore.sql
@@ -36,6 +36,31 @@ CREATE TABLE `books` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+DROP TABLE IF EXISTS `users`;
+
+-- Placeholder for user profile table
+CREATE TABLE `users` (
+    `user_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `userpassword` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `address` VARCHAR(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+DROP TABLE IF EXISTS `rate_comment`;
+
+CREATE TABLE `rate_comment` (
+    `ra_co_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NOT NULL,
+    `isbn` VARCHAR(255) NOT NULL,
+    `rating` DECIMAL(2, 1) CHECK (rating BETWEEN 0 AND 5) DEFAULT NULL,
+    `comment` TEXT DEFAULT NULL,
+    `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`isbn`) REFERENCES `books`(`isbn`) ON DELETE CASCADE,
+    UNIQUE KEY (`user_id`, `isbn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
 --
 -- Dumping data for table `books`
 --
